dd1b88cb305fc1134937760e0bffd06096e23495
mag <maguilamo.c@gmail.com>
2017-07-05 20:16:49 -0400

    Code rearrangement
    Rearrange code so it follows a gem-like behaviour.
    
    Now API is expose through the module Curry.

--- lib/scraper/scraper.rb
+++ lib/scraper/scraper.rb
@@ -1,6 +1,17 @@
-require "curry/scraper/info"
+require 'mechanize'
 
 module Scraper
+
+  #=> Mechanize agent
+  def self.initialize_agent
+    agent                        = Mechanize.new
+    agent.user_agent             = USER_AGENTS.sample
+    agent.ssl_version            = :TLSv1
+    agent.agent.http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+    agent
+  end
+
   USER_AGENTS = ['Mozilla/5.0 (Macintosh; Intel Mac OS X 10_6_8) AppleWebKit/537.13+ (KHTML, like Gecko) Version/5.1.7 Safari/534.57.2',
                  'Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.2 Safari/537.36',
                  'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:21.0) Gecko/20130331 Firefox/21.0',
---
+++ lib/version.rb
@@ -0,0 +1,4 @@
+module Curry
+  # remove Info and include Scraper into Curry
+  VERSION = "2.2.0"
+end
